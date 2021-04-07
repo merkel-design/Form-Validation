79006d51c2fe43c19c64e577babda451ebb52ba3
Merkel Design <alexmerkelphotography@gmail.com>
2021-04-07 10:46:34 -0500

    added regex for email nd adding check box toggler

--- formValidation.js
+++ formValidation.js
@@ -21,12 +21,12 @@ function checkInputs() {
 
  
 
-    const regex = ['!','#','$','%','&',"'",'*','+','-','/','=','?','^','_',"`","{","}","|","]","[","(",")"];
     
     
     // USERNAME
     if (usernameValue.length > 3){
         successForm(username);
+        //, username-grid-item-parent
     }
     else if (usernameValue.length <= 3){
         errorForm(username, "Your username must be longer than 3 letters");
@@ -34,40 +34,38 @@ function checkInputs() {
     
     // EMAIL
 
-    // let beforeAtSign = ;
-    // let afterAtSign = ;
-
-    for (i=0; i < emailValue.length; i++) {
-        
-        if (emailValue[0] === regex[i] || emailValue[emailValue.length - 1] === regex[i]) {
-            errorForm(email, `"${regex[i]}" cannot be the first or last character in an email address. Please try again.`);
-        }
-
-        else if (emailValue.length >= 64) {
-            errorForm(email, "Your email must be less than 64 characters.");
-        }
-
-        else if (emailValue.length >= 64) {
-            errorForm(email, "Your email must be less than 64 characters.");
-        }
-        else if (emailValue[i] && emailValue[i+1] === regex[i]) {
-            errorForm(email, "Your email cannot have consecutive special characters.");
-        }
-        // else if (emailValue){
-        //     successForm(email)
-        // }
-        
+    if (emailValue === '') {
+        errorForm(email, "Your email cannot be blank.");
+    }
+    else if (!validateEmail(emailValue)){
+        errorForm(email, "Email is not valid");
+    }
+    else {
+        successForm(email)
     };
+
 
 // End of function
 };
 
 
-function successForm(inputID) {
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+
+function successForm(inputID, checkmarkParent) {
     //adds the green border 
     //then if the error class was added before this instant, the error class is removed
     inputID.classList.remove("grid-form-item-position-error");
     inputID.classList.add("grid-form-item-position-success");
+
+    //username-grid-item-parent
+
+    // checkmarkParent.i.classList.remove("hiding-toggle");
+
+    
     
 };
 
