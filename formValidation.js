5f48e8445b7070e0fd8ea3fb2102e126e3622e1f
Merkel Design <alexmerkelphotography@gmail.com>
2021-04-06 20:09:39 -0500

    email validation

--- formValidation.js
+++ formValidation.js
@@ -19,23 +19,62 @@ function checkInputs() {
     const passwordValue = password.value.trim();
     const password2Value = password2.value.trim();
 
+ 
+
+    const regex = ['!','#','$','%','&',"'",'*','+','-','/','=','?','^','_',"`","{","}","|","]","[","(",")"];
     
+    
+    // USERNAME
     if (usernameValue.length > 3){
         successForm(username);
     }
     else if (usernameValue.length <= 3){
         errorForm(username, "Your username must be longer than 3 letters");
     }
+    
+    // EMAIL
 
+    // let beforeAtSign = ;
+    // let afterAtSign = ;
+
+    for (i=0; i < emailValue.length; i++) {
+        
+        if (emailValue[0] === regex[i] || emailValue[emailValue.length - 1] === regex[i]) {
+            errorForm(email, `"${regex[i]}" cannot be the first or last character in an email address. Please try again.`);
+        }
+
+        else if (emailValue.length >= 64) {
+            errorForm(email, "Your email must be less than 64 characters.");
+        }
+
+        else if (emailValue.length >= 64) {
+            errorForm(email, "Your email must be less than 64 characters.");
+        }
+        else if (emailValue[i] && emailValue[i+1] === regex[i]) {
+            errorForm(email, "Your email cannot have consecutive special characters.");
+        }
+        // else if (emailValue){
+        //     successForm(email)
+        // }
+        
+    };
+
+// End of function
 };
 
 
 function successForm(inputID) {
-    inputID.classList.add("grid-form-item-position-success")
+    //adds the green border 
+    //then if the error class was added before this instant, the error class is removed
+    inputID.classList.remove("grid-form-item-position-error");
+    inputID.classList.add("grid-form-item-position-success");
+    
 };
 
 function errorForm(inputID, message) {
-    inputID.classList.add("error")
-    console.log(message)
+    //adds the red border
+    inputID.classList.add("grid-form-item-position-error");
+    inputID.classList.remove("grid-form-item-position-success");
+    console.log(message);
 };
 
